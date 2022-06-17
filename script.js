2114d2a4a103e70030477a3c07fb24f0b1fc72ea
Ahmed Bule <ahmedbule01@gmail.com>
2022-06-17 11:17:29 -0500

    created a single meal function

--- script.js
+++ script.js
@@ -5,3 +5,21 @@ const search = document.getElementById('search'),
     resultHeaeding = document.getElementById('result-heading'),
     single_mealEl = document.getElementById('single-meal'),
 
+
+//Search Meal and fetch from api
+
+function searchMeal(e) {
+    e.preventDefault();
+
+    //Clear single meal
+    single_mealEl.innerHTML = " ";
+
+    //Get seach term
+    const term = search.value
+
+    console.log(term)
+}
+
+
+// Event Listener
+submit.addEventListener('submit', seachMeal)
